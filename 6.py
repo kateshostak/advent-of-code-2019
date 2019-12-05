3f1b0b662626440a8069a2864d42d3ce40ab61da
Trina <31892809+kateshostak@users.noreply.github.com>
2019-12-05 19:37:18 +0300

    8th solution

--- 6.py
+++ 6.py
@@ -1,21 +1,23 @@
+import pdb
+
 def count_passwords(start, end):
     count = 0
     for k in range(start, end + 1):
         passwrd = str(k)
         if is_non_decreasing(passwrd):
-            #pdb.set_trace()
+       #     pdb.set_trace()
             for i in range(10):
-                if find_substring(i, passwrd):
+                if find_substr(i, passwrd) == 2:
                     count += 1
                     break
     return count
 
-def find_substring(i, passwrd):
+def find_substr(i, passwrd):
     for j in range(6, 1, -1):
         substr = str(i)*j
         if substr in passwrd:
-            return True
-    return False
+            return j
+    return 0
 
 def is_non_decreasing(passwrd):
     for i in range(len(passwrd) - 1):
