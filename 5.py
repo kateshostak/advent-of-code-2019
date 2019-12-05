081b2d49bb46db053dcc078975b47361a2d7c99c
Trina <31892809+kateshostak@users.noreply.github.com>
2019-12-05 18:17:38 +0300

    6th solution

--- 5.py
+++ 5.py
@@ -1,3 +1,6 @@
+import pdb
+
+
 class Wire():
     def __init__(self, arr):
         self.directions = {
@@ -7,6 +10,30 @@ class Wire():
                 'R': self.go_right
                 }
         self.wire = self.make_wire(arr)
+
+    def count_steps(self, point):
+        position = [0, 0]
+        steps = 0
+        for i in range(len(self.wire) - 1):
+            line = [self.wire[i], self.wire[i + 1]]
+            if self.point_in_line(line, point):
+                #pdb.set_trace()
+                tmp_steps = manhattan_distance(position, point)
+                steps += tmp_steps
+                return steps
+            old_position, position = position, self.wire[i+1]
+            steps += manhattan_distance(old_position, position)
+        return None
+
+    def point_in_line(self, line, point):
+        x_start = min(line[0][0], line[1][0])
+        x_end = max(line[0][0], line[1][0])
+        y_start = min(line[0][1], line[1][1])
+        y_end = max(line[0][1], line[1][1])
+        if point[0] >= x_start and point[0] <= x_end:
+            if point[1] >= y_start and point[1] <= y_end:
+                return True
+        return False
 
     def cross(self, wire):
         cross_points = []
@@ -43,8 +70,8 @@ class Wire():
             cross_points.append(point)
 
     def make_wire(self, arr):
-        wire = [[0,0]]
-        position = [0,0]
+        wire = [[0, 0]]
+        position = [0, 0]
         for elem in arr:
             direction = elem[0]
             value = int(elem[1:])
@@ -81,8 +108,25 @@ class Wire():
     def __len__(self):
         return len(self.wire)
 
+
 def manhattan_distance(point1, point2):
-    return abs(point2[0] - point1[0]) + abs(point2[1] - point1[0])
+    return abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
+
+def minimize_signal(steps1, steps2):
+    step_sum = []
+    for elem in zip(steps1, steps2):
+        tmp_sum = elem[0] + elem[1]
+        step_sum.append(tmp_sum)
+    return min(step_sum)
+
+
+def count_steps(wire, points):
+    steps = []
+    for point in points:
+        res = wire.count_steps(point)
+        steps.append(res)
+    return steps
+
 
 def main():
     paths = []
@@ -93,14 +137,28 @@ def main():
             for elem in tmp:
                 path.append(elem)
             paths.append(path)
+
     wire1 = Wire(paths[0])
     wire2 = Wire(paths[1])
+    wires = [wire1, wire2]
+
     cross_points = wire1.cross(wire2)
     centr_port = [0, 0]
     manh_dist = []
     for point in cross_points:
         manh_dist.append(manhattan_distance(centr_port, point))
     print(min(manh_dist))
+
+    steps = []
+    for i in range(2):
+        tmp_steps = count_steps(wires[i], cross_points)
+        steps.append(tmp_steps)
+
+    print(steps[0])
+    print(steps[1])
+    res = minimize_signal(steps[0], steps[1])
+    print(res)
+
 
 if __name__ == '__main__':
     main()
