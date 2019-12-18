8ffe6962d96565c205d22a2f5c423cb8f5dfa086
Trina <31892809+kateshostak@users.noreply.github.com>
2019-12-18 19:45:53 +0300

    Add something relatively working

--- 27.py
+++ 27.py
@@ -1,5 +1,7 @@
-import time
 import pdb
+import sys
+import asyncio
+import time
 from Comp import Memmory
 from Comp import get_input_prog
 from prog import Prog
@@ -7,8 +9,10 @@ from point import point
 
 
 def store_input(INPUTS):
-    return INPUTS.pop(0)
-
+    if INPUTS:
+        return INPUTS.pop(0)
+    else:
+        return 0
 
 def print_output(i, OUTPUTS):
     OUTPUTS.append(i)
@@ -65,6 +69,16 @@ class Canvas():
         self.update_field(canvas)
         self.draw_field()
 
+
+def user_input(INPUTS):
+    inpt = sys.stdin.read(1)
+    if inpt == 'a':
+        INPUTS.append(-1)
+    if inpt == 'd':
+        INPUTS.append(1)
+    else:
+        INPUTS.append(0)
+
 def main():
     arr = get_input_prog('26.txt')
     prog = Prog(arr)
@@ -72,15 +86,23 @@ def main():
     INPUTS = []
     OUTPUTS = []
     prog.set_value(0, 2)
-    intcode = Memmory(prog, input, print_output, INPUTS, OUTPUTS)
+    intcode = Memmory(prog, store_input, print_output, INPUTS, OUTPUTS)
 
     to_exit = 'Resume'
     canvas = None
+    loop = asyncio.get_event_loop()
+    loop.add_reader(sys.stdin, user_input, INPUTS)
     while to_exit != 'Stop':
-        to_exit = intcode.compute()
+        to_exit = loop.run_until_complete(intcode.compute())
+
         if len(OUTPUTS)%3 == 0:
             canvas = Canvas(OUTPUTS)
-            canvas.draw_field()
-
+            if to_exit == 'Input':
+                canvas.draw_field()
+                time.sleep(0.1)
+                canvas1 = Canvas(OUTPUTS)
+                canvas1.draw_field()
+            if len(OUTPUTS) > 2283:
+                time.sleep(0.1)
 if __name__ == '__main__':
     main()
