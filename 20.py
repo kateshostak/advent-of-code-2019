6cb800e244f5d4df2735c3a67933f69a39608173
Trina <31892809+kateshostak@users.noreply.github.com>
2019-12-23 19:02:00 +0300

    Add properly working blocking for one-quarter of the field

--- 20.py
+++ 20.py
@@ -1,41 +1,62 @@
 import pdb
 from collections import namedtuple
 
-def block(arr2, point, step_i, step_j, length, width):
-    i = point.x + step_i*2
-    j = point.y + step_j*2
-    while i < length and j < width:
+
+def block(arr2, point, step_i, step_j, columns, rows):
+    i = point.row + step_i
+    j = point.column + step_j
+    #pdb.set_trace()
+    while i < rows and j < columns:
         arr2[i][j] = 0
         i += step_i
         j += step_j
 
-def fourth_quarter(arr, point, length, width):
+
+def count_asteroids(arr, point):
+    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+
+
+def count_quarter(arr, point, step_row, step_column, columns, rows):
     asteroids = 0
-    i = 1
-    j = 1
-    while i < width:
-        j = 1
-        while j < length:
-            if arr[point.x + i][point.y + j] == '#':
+    row = point.row + step_row
+    #pdb.set_trace()
+    while row != rows:
+        column = point.column + step_column
+        while column != columns:
+            if arr[row][column] == '#':
                 asteroids += 1
-                block(arr, point, i, j, length, width)
-            j += 1
-        i += 1
+                step_i = row - point.row
+                step_j = column - point.column
+                block(arr, point, step_i, step_j, columns, rows)
+            column  += step_column
+        row += step_row
     return asteroids
 
-def  main():
-    point = namedtuple('Point', ['x', 'y'])
+
+def main():
+    point = namedtuple('Point', ['row', 'column'])
     a = point(0, 0)
+    a = point(0, 1)
+    a = point(0, 2)
+    a = point(0, 3)
+    a = point(1, 1)
+    a = point(2, 2)
+
     arr = [
-            ['#', '1', '2', '3'],
-            ['4', '#', '#', '#'],
-            ['8', '9', '10', '11'],
-            ['12', '13', '14', '15']
+            ['#', 'x', 'x', 'x'],
+            ['x', '#', '#', '#'],
+            ['x', 'x', '#', 'x'],
+            ['x', 'x', 'x', 'x']
             ]
-    arr2 = arr.copy()
-    print(fourth_quarter(arr, a, 4, 4))
-    for row in arr2:
-        print(*row)
+    arr[a.row][a.column] = '*'
+    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+
+    for row in arr:
+        print(*row) # noqa
+
 
 if __name__ == '__main__':
     main()
