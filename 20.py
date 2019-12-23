b269e1a0f26e74ceca50df9b5959edd17d9256c4
Trina <31892809+kateshostak@users.noreply.github.com>
2019-12-23 19:31:06 +0300

    Fix the blocking function to work with any quarter of the field

--- 20.py
+++ 20.py
@@ -2,21 +2,37 @@ import pdb
 from collections import namedtuple
 
 
-def block(arr2, point, step_i, step_j, columns, rows):
+def block(arr2, point, step_j, step_i, columns, rows):
     i = point.row + step_i
     j = point.column + step_j
     #pdb.set_trace()
-    while i < rows and j < columns:
-        arr2[i][j] = 0
-        i += step_i
-        j += step_j
+    if step_i > 0 and step_j > 0:
+        while i < rows and j < columns:
+            arr2[i][j] = 'o'
+            i += step_i
+            j += step_j
+    elif step_i < 0 and step_j > 0:
+        while i > rows and j < columns:
+            arr2[i][j] = 'o'
+            i += step_i
+            j += step_j
+    elif step_i > 0 and step_j < 0:
+        while i < rows and j > columns:
+            arr2[i][j] = 'o'
+            i += step_i
+            j += step_j
+    else:
+        while i > rows and j > columns:
+            arr2[i][j] = 'o'
+            i += step_i
+            j += step_j
 
 
 def count_asteroids(arr, point):
     as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
 
 
-def count_quarter(arr, point, step_row, step_column, columns, rows):
+def count_quarter(arr, point, step_column, step_row, columns, rows):
     asteroids = 0
     row = point.row + step_row
     #pdb.set_trace()
@@ -27,7 +43,7 @@ def count_quarter(arr, point, step_row, step_column, columns, rows):
                 asteroids += 1
                 step_i = row - point.row
                 step_j = column - point.column
-                block(arr, point, step_i, step_j, columns, rows)
+                block(arr, point, step_j, step_i, columns, rows)
             column  += step_column
         row += step_row
     return asteroids
@@ -35,24 +51,18 @@ def count_quarter(arr, point, step_row, step_column, columns, rows):
 
 def main():
     point = namedtuple('Point', ['row', 'column'])
-    a = point(0, 0)
-    a = point(0, 1)
-    a = point(0, 2)
     a = point(0, 3)
-    a = point(1, 1)
-    a = point(2, 2)
-
     arr = [
             ['#', 'x', 'x', 'x'],
             ['x', '#', '#', '#'],
-            ['x', 'x', '#', 'x'],
+            ['x', '#', '#', 'x'],
             ['x', 'x', 'x', 'x']
             ]
     arr[a.row][a.column] = '*'
-    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
-    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
-    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
-    as1 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+    #as4 = count_quarter(arr, a, 1, 1, len(arr[0]), len(arr))
+    #as1 = count_quarter(arr, a, 1, -1, len(arr[0]), -1)
+    #as2 = count_quarter(arr, a, -1, -1, -1, -1)
+    as3 = count_quarter(arr, a, -1, 1, -1, len(arr))
 
     for row in arr:
         print(*row) # noqa
